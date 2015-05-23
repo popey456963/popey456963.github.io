49158179d842060d0672e087b70f2a13d2ffed6b
Popey Gilbert <popey@debenclipper.com>
2015-05-23 14:41:47 +0100

    FirstPage

---
+++ FirstPage/test.js
@@ -0,0 +1,12 @@
+var person = {
+	firstname: "John",
+	lastname: "Doe",
+	age: 50,
+	eyecolor: "blue"
+};
+document.getElementById("demo").innerHTML = person.firstname + " is " + person.age + " years old.";
+
+function hello() {
+	console.log("Hello")
+}
+hello()
