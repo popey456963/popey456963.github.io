b13da571faff77d5e1f737e0304f0adf675ac4a7
Popey Gilbert <popey@debenclipper.com>
2015-08-06 22:11:11 +0100

    Beautiful Names

--- p/videosort/index.js
+++ p/videosort/index.js
@@ -1,3 +1,6 @@
+var j = 0;
+var newArray = [];
+
 function getJSON() {
     $.getJSON("https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics&id=Y_frQ3VoT4g,O0I6dwnM-Ho,DHT1c6GNk1o,2BrWFylB_nE,6k4UQkagOGA,aDDoOIfecKk,B91Fxd9oe0c,XcgCS_FT0QU,00JQRue9r-Y,d5zFURISLgA,X00LET30gLM,jU4CR31Ekdg,XrNuE3-p2VM,LJlT4aHRFR8,CobwJUP2AIA,kPatFT_3ziU&key=AIzaSyAOa0igcKcrRTVkk_NTNEha8Ch7bOAe7j8", function(data) {
         var ytData = data.items;
@@ -8,19 +11,45 @@ function getJSON() {
                 views: parseFloat(ytData[i].statistics.viewCount)
             });
         }
-        printData(array);
+        sortData(array);
     })
 }
 
-function printData(array) {
+function sortData(array) {
     array.sort(function(a, b) {
         return a.views - b.views;
     });
+    getNames(array);
+}
 
+function loadInfo(array) {
+    $.getJSON("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + array.id + "&key=AIzaSyAOa0igcKcrRTVkk_NTNEha8Ch7bOAe7j8", function(data) {
+        j = j + 1;
+        var ytTitleData = data.items[0].snippet.title;
+        // console.log(ytTitleData);
+        newArray.push({
+            id: array.id,
+            name: ytTitleData.substring(26),
+            views: array.views
+        });
+        if (j == 16) {
+        	printData(newArray);
+        }
+    });
+
+}
+
+function getNames(array) {
+    for (i = 0; i < array.length; i++) {
+        loadInfo(array[i]);
+    }
+}
+
+function printData(array) {
     Morris.Bar({
         element: 'bar-example',
         data: array,
-        xkey: 'id',
+        xkey: 'name',
         ykeys: ['views'],
         labels: ['ID']
     });
