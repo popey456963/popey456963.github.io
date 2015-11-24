dae566a8a1969f8b4db7622d96fa02f0edd64fef
Popey Gilbert <popey@debenclipper.com>
2015-11-24 20:19:02 +0000

    homepage

--- p/homepage/js/google.js
+++ p/homepage/js/google.js
@@ -1,26 +1,10 @@
-var q = document.getElementById("q");
 $("#q").keypress(function(e) {
     var textVal = $(this).val();
     if(e.which === 13 && e.shiftKey) {
-    	wolfram(e);
+        window.location.href = "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(textVal);
     } else if (e.which === 10) {
-    	wikipedia(e);
+        window.location.href = "https://en.wikipedia.org/wiki/Special:Search?go=Go&search=" + encodeURIComponent(textVal);
     } else if (e.which === 13) {
-    	validate(e);
+    	window.location.href = "https://www.google.co.uk?#safe=strict&q=" + encodeURIComponent(textVal);
     }
 });
-
-function validate(e) {
-    var text = q.value;
-    window.location.href = "https://www.google.co.uk?#safe=strict&q=" + encodeURIComponent(text);
-}
-
-function wolfram(e) {
-	var text = q.value;
-	window.location.href = "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(text);
-}
-
-function wikipedia(e) {
-	var text = q.value;
-	window.location.href = "https://en.wikipedia.org/wiki/Special:Search?go=Go&search=" + encodeURIComponent(text);
-}
